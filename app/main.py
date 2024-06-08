e10faddf2619cb4a11a1cccb32bccf9e07c2a6c4
Mridul Pant <mridulpant2010@gmail.com>
2024-06-08 20:17:30 +0530

    added code for reading the data from git repo

--- app/main.py
+++ app/main.py
@@ -2,31 +2,29 @@ import os
 import zlib
 import argparse
 
+
 def read_file_path(sha_objectname):
     git_prefix = ".git/objects"
     file_path = f"{git_prefix}/{sha_objectname[:2]}/{sha_objectname[2:]}"
-    print(file_path)
     try:
         with open(file_path, "rb") as f:
             return zlib.decompress(f.read())
-    except (NotADirectoryError,FileNotFoundError):
+    except (NotADirectoryError, FileNotFoundError):
         return None
-    
+
+
 def get_file_content(read_blob_object):
     # how shall i get the contents from the blob object
-    result= read_blob_object.split(b"\x00")
-    return result[1]
-    
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    result = read_blob_object.split(b"\x00")
+    return result[-1]
 
-    # Uncomment this block to pass the first stage
-    #
-    
+
+def main():
     parser = argparse.ArgumentParser(description="A simple CLI example.")
     parser.add_argument("command", action="store", help="input git helper commands")
-    parser.add_argument("-p",dest='hash_value' ,action="store",type=str, help="enable p flag")
+    parser.add_argument(
+        "-p", dest="hash_value", action="store", type=str, help="enable p flag"
+    )
 
     args = parser.parse_args()
 
@@ -36,16 +34,12 @@ def main():
         os.mkdir(".git/refs")
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
-        print("Initialized git directory")
     elif args.command == "cat-file":
         sha_value = args.hash_value
-        # print("sha value is:",sha_value)
         read_blob_object = read_file_path(sha_value)
-        # print(" fetching the file blob object")
-        # print(read_blob_object)
         if read_blob_object:
-            file_contents=get_file_content(read_blob_object)
-            print(file_contents.decode('utf-8'))
+            file_contents = get_file_content(read_blob_object)
+            print(file_contents.decode("utf-8"), end="")
     else:
         raise RuntimeError(f"Unknown command #{args.command}")
 
